2fa9bd15826a448707dc9736a78d9220d446a988
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-09-07 00:32:51 +0530

    done user get part

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+
+const Users = require('../models/users.models')
+const User = require("../models/users.models");
+app.use(express.json())
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.find();
+        res.json(users)
+    } catch (err) {
+        res.send("Err : " + err)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.findById(req.params.id)
+        res.json(user)
+    } catch (err) {
+        res.send('Err : ' + err)
+    }
+})
