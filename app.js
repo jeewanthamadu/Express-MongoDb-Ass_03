934fd83aa563206d9ae2b1787d4319988ce6f01d
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-09-06 23:26:20 +0530

    done basic config

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 4000
+
+const url = 'mongodb://localhost/express'
+
+mongoose.connect(url, {useNewUrlParser: true})
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log("MongoDB Connected")
+})
+
+app.use(express.json())
+
+
+app.listen(port, () => {
+    console.log(`app starting on ${port}`);
+})
