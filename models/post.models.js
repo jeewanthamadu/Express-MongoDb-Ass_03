20a706dbbb286aad606dc8c7d42fa205d02c1722
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-09-06 23:59:29 +0530

    create post table

---
+++ models/post.models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+
+const postSchema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required: true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    body:{
+        type:String,
+        required:true,
+        trim:true
+    }
+})
+
+module.exports = mongoose.model('post',postSchema)
